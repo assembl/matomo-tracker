4dd4e711928b7edc3e50280bec3f524171855d7f
Romain Testard <romain.testard@bluenove.com>
2020-10-16 16:00:04 +0200

    I have moved libs to proper folders

---
+++ matomo-tracker-react/lib/MatomoContext.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { MatomoInstance } from './types';
+declare const MatomoContext: React.Context<MatomoInstance>;
+export default MatomoContext;
+//# sourceMappingURL=MatomoContext.d.ts.map
---
+++ matomo-tracker-react/lib/utils/useOutboundClickListener.d.ts
@@ -0,0 +1,4 @@
+import { MatomoInstance } from '../types';
+declare const useOutboundClickListener: ({ trackLink }: MatomoInstance) => void;
+export default useOutboundClickListener;
+//# sourceMappingURL=useOutboundClickListener.d.ts.map
